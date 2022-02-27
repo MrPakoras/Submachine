893006624cce71dadcee2f1341dee770aa5d555d
MrPakoras <hartanas8g@gmail.com>
2022-02-27 17:27:35 +0000

    Update Submachine GUI v0.1.py

--- Submachine GUI v0.1.py
+++ Submachine GUI v0.1.py
@@ -8,11 +8,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 from tkvideo import tkvideo
 
+bgcolour = '#1d1c2c'
+
 master = Tk()
 master.title('Submachine GUI v0.1')
 master.geometry('1280x720')
 # master.resizable(False, False)
-master.configure(background='#1d1c2c')
+master.configure(background=bgcolour)
 master.columnconfigure(0, weight=2)
 # icon = PhotoImage(file='./extras/icon.png')
 # master.iconphoto(False, icon)
@@ -21,10 +23,10 @@ master.columnconfigure(0, weight=2)
 
 ## File selection
 
-topframe = Frame(master)
-topframe.grid(row=0, column=0)
+topframe = Frame(master, bg=bgcolour)
+topframe.grid(row=0, column=0, sticky='w', padx=20, pady=15)
 
-bottomframe = Frame(master)
+bottomframe = Frame(master, bg=bgcolour)
 bottomframe.grid(row=1, column=0)
 
 ## Browse for file function
@@ -53,48 +55,97 @@ def browse(type):
 
 ## Address bar and browse button
 
-leftsubframe = Frame(topframe) # Frame for video preview and controls
-leftsubframe.grid(row=0, column=0)
+leftsubframe = Frame(topframe, bg=bgcolour) # Frame for video preview and controls
+leftsubframe.grid(row=0, column=0, sticky='w')
 
-# Browse Video
-bvframe = Frame(leftsubframe, width=448, bg='#1d1c2c')
+# Video
+bvframe = Frame(leftsubframe, width=448, bg=bgcolour) # browse video frame
 bvframe.pack_propagate(0)
-bvframe.grid(row=0, column=0)
+bvframe.grid(row=0, column=0, sticky='w')
+
+bvlabel = Label(bvframe, text='Video File:', bg=bgcolour, fg='#d7ceff', font=('Helvetica', 18, 'bold'))
+bvlabel.grid(row=0, column=0, sticky='w')
 
 bvaddrvar = StringVar(bvframe)
 bvvar = 'Please choose a video file...'
 bvaddrvar.set(bvvar)
 
 bvaddrlab = Label(bvframe, textvariable=bvaddrvar, anchor='w', width=40, bg='#1d1c2c', fg='#d7ceff')
-bvaddrlab.grid(row=0, column=0, sticky='we')
+bvaddrlab.grid(row=1, column=0, sticky='we')
 
 bvbutton = Button(bvframe, text='Browse', command=lambda:browse('video'), width=9, bg='#1d1c2c', fg='#8d73ff', activebackground='#1d1c2c' , activeforeground='#8d73ff') # Browse video button
+# Lambda function so command can be run with parameters
 bvbutton.config(state='normal')
-bvbutton.grid(row=0, column=1, pady=4)
+bvbutton.grid(row=1, column=1, pady=5)
 
-# Browse Audio
-baframe = Frame(leftsubframe, width=448, bg='#1d1c2c')
+mvvar = IntVar() # Mute video variable
+mvvar.set(0)
+mvcheck = Checkbutton(bvframe, text='Mute video', variable=mvvar, bg='#1d1c2c', fg='#8d73ff', activebackground='#1d1c2c' , activeforeground='#8d73ff') # Mute video check button
+mvcheck.grid(row=2, column=0, padx=4, sticky='w')
+
+
+# Breaker
+leftsubframe.columnconfigure(1, weight=2)
+
+
+# Audio
+baframe = Frame(leftsubframe, width=448, bg=bgcolour) # Browse audio frame
 baframe.pack_propagate(0)
-baframe.grid(row=1, column=0)
+baframe.grid(row=2, column=0)
+
+balabel = Label(baframe, text='Audio File:', bg=bgcolour, fg='#d7ceff', font=('Helvetica', 18, 'bold'))
+balabel.grid(row=0, column=0, sticky='w')
 
 baaddrvar = StringVar(baframe)
 bavar = 'Please choose an audio file...'
 baaddrvar.set(bavar)
 
 baaddrlab = Label(baframe, textvariable=baaddrvar, anchor='w', width=40, bg='#1d1c2c', fg='#d7ceff')
-baaddrlab.grid(row=0, column=0, sticky='we')
+baaddrlab.grid(row=1, column=0, sticky='we')
 
 babutton = Button(baframe, text='Browse', command=lambda:browse('audio'), width=9, bg='#1d1c2c', fg='#8d73ff', activebackground='#1d1c2c' , activeforeground='#8d73ff') # Browse video button
 babutton.config(state='normal')
-babutton.grid(row=0, column=1, pady=4)
+babutton.grid(row=1, column=1, pady=5)
 
+vslider = Scale(baframe, label='Volume', from_=0, to=200, orient=HORIZONTAL, length=350, bg='#8d73ff', fg='#1d1c2c', activebackground='#8d73ff', troughcolor='#d7ceff') # volume slider
+vslider.set(100)
+vslider.grid(row=2, column=0)
+
+
+# Breaker
+leftsubframe.columnconfigure(3, weight=2)
+
+
+# Subtitles
+subsframe = Frame(leftsubframe, width=448, bg=bgcolour) # Subtitles frame
+subsframe.pack_propagate(0)
+subsframe.grid(row=4, column=0, sticky='w', pady=10)
+
+subslabel = Label(subsframe, text='Subtitles:', bg=bgcolour, fg='#d7ceff', font=('Helvetica', 18, 'bold'))
+subslabel.grid(row=0, column=0, sticky='w')
+
+
+# def addsub(): # Function to create new subtitles
+# 	baaddrvar = StringVar(baframe)
+# 	bavar = 'Please choose an audio file...'
+# 	baaddrvar.set(bavar)
+
+# 	baaddrlab = Label(baframe, textvariable=baaddrvar, anchor='w', width=40, bg='#1d1c2c', fg='#d7ceff')
+# 	baaddrlab.grid(row=1, column=0, sticky='we')
+
+# 	babutton = Button(baframe, text='Browse', command=lambda:browse('audio'), width=9, bg='#1d1c2c', fg='#8d73ff', activebackground='#1d1c2c' , activeforeground='#8d73ff') # Browse video button
+# 	babutton.config(state='normal')
+# 	babutton.grid(row=1, column=1, pady=5)
+
+subscanvas = Canvas(subsframe, bg='#d7ceff')
+subscanvas.grid(row=1, column=0, sticky='w')
 
 
 ## Video preview and controls
-rightsubframe = Frame(topframe) # Frame for video preview and controls
+rightsubframe = Frame(topframe, bg=bgcolour) # Frame for video preview and controls
 rightsubframe.grid(row=0, column=1)
 
-vidframe = Frame(rightsubframe) # Frame for video
+vidframe = Frame(rightsubframe, bg=bgcolour) # Frame for video
 vidframe.grid(row=0, column=0)
 
 ## Video Preview
